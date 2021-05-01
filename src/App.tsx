cbd945b64fbd22b2733cd5364a82efc11541baf7
Anton Shestakov <prradd@gmail.com>
2021-05-02 01:30:09 +0300

    Fants for Stas

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {fantsData} from './Data/fants'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [randomFant, setRandomFant] = useState("");
+
+    const HandleClick = () => {
+        const randomNum = Math.floor(Math.random() * fantsData.length);
+        setRandomFant(fantsData[randomNum]?.fantText);
+    }
+
+    return (
+        <div className="App">
+            <div className="fant-text">
+                {randomFant}
+            </div>
+            <button
+                id="spin"
+                className="neon-button"
+                onClick={() => HandleClick()}
+            >Верти!</button>
+        </div>
+    );
 }
 
 export default App;
